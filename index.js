79106151c41eee0bd3fe6e37df3d2041e087e34b
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-11 19:13:44 -0400

    commit tree 9ca3ef32bea44f70d5a00f226fa1c873c8032603

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+    let scuberHQ = 42;
+    return Math.abs(scuberHQ - someValue); 
+}
+
+function distanceFromHqInFeet (someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice (start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+    if (distanceTravelled <= 400){
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled < 2000){
+        return (distanceTravelled - 400) * 0.02
+    } else if (distanceTravelled > 2000 && distanceTravelled < 2500){
+        return 25;
+    } else {
+        return `cannot travel that far`;
+    }
+}
